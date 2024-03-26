b520e647b06c411a340e82c7de9b5fb0e07f347d
Ryan <vellabonelloryan@gmail.com>
2024-03-26 10:52:02 +0100

    feat: user can edit profile

--- app/Livewire/EditProfile.php
+++ app/Livewire/EditProfile.php
@@ -18,21 +18,51 @@ class EditProfile extends Component implements HasForms
 
     public function mount(): void
     {
-        $this->form->fill();
+        $this->form->fill(auth()->user()->toArray());
     }
 
     public function form(Form $form): Form
     {
         return $form
             ->schema([
-                //
+                Forms\Components\Grid::make(3)
+                    ->schema([
+                        Forms\Components\Section::make()
+                            ->schema([
+                                Forms\Components\TextInput::make('name')
+                                    ->required(),
+                                Forms\Components\FileUpload::make('avatar')
+                                    ->image()
+                                    ->panelLayout('circle')
+                                    ->maxSize(1024),
+                            ])
+                            ->columnSpan(1),
+                        Forms\Components\Section::make()
+                            ->schema([
+                                Forms\Components\TextArea::make('bio')
+                                    ->columnSpanFull(),
+                                Forms\Components\TextInput::make('sm_twitter')
+                                    ->label('twitter')
+                                    ->prefix('https://twitter.com/'),
+                                Forms\Components\TextInput::make('sm_linkedin')
+                                    ->label('linkedin')
+                                    ->prefix('https://www.linkedin.com/in/'),
+                                Forms\Components\TextInput::make('sm_instagram')
+                                    ->label('instagram')
+                                    ->prefix('https://www.instagram.com/'),
+                            ])
+                            ->columns(2)
+                            ->columnSpan(2),
+                    ])
             ])
             ->statePath('data');
     }
 
     public function update(): void
     {
-        dd($this->form->getState());
+        $user = auth()->user();
+
+        $user->update($this->form->getState());
     }
 
     public function render(): View
--- resources/views/livewire/edit-profile.blade.php
+++ resources/views/livewire/edit-profile.blade.php
@@ -1,16 +1,14 @@
 <div class="container py-12">
 
-    <div class="text-center mb-12">
-        <h1 class="text-gray-800 text-5xl font-bold mb-8">Edit your profile</h1>
-        <p class="text-gray-600 text sm:text-lg md:text-xl">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-    </div>
-
-    <form wire:submit="update" class="mx-auto max-w-3xl">
+    <form wire:submit="update" class="mx-auto">
+        <div class="flex justify-between mb-6">
+            <h2 class="text-3xl font-bold text-gray-800">Edit your profile</h2>
+            <div class="gap-2">
+                <button type="submit" class="btn btn-primary">Save changes</button>
+                <a href="" class="btn btn-muted">View profile</a>
+            </div>
+        </div>
         {{ $this->form }}
-        
-        <button type="submit" class="mt-6 btn btn-xl btn-primary">
-            Save changes
-        </button>
     </form>
     
     <x-filament-actions::modals />
